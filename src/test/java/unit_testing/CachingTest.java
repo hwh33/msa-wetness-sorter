b1f57dce930bb23c8aed1fa763502113672f57e8
Harry Harpham <hwh33@cornell.edu>
2015-09-30 20:55:07 -0500

    Add caching test

---
+++ src/test/java/unit_testing/CachingTest.java
@@ -0,0 +1,32 @@
+package unit_testing;
+
+import org.junit.Test;
+
+public class CachingTest {
+	
+	@Test
+	public void createNewCacheFromURL() {
+		// TODO: implement me
+	}
+	
+	@Test
+	public void createNewCacheFromAPIRequests() {
+		// TODO: implement me
+	}
+	
+	@Test
+	public void loadExistingCache() {
+		// TODO: implement me
+	}
+
+	@Test
+	public void loadCacheFileAndRead() {
+		// TODO: implement me
+	}
+	
+	@Test
+	public void refreshCache() {
+		// TODO: implement me
+	}
+
+}
